a30ed5344b0cfcbf78e88b2bbecbb38733d2e8fd
amandasuzzanne <amandasuzanne12@gmail.com>
2024-04-23 15:06:52 +0300

    Complete lab

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import TestComponent from "./TestComponent";
 
 // no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
     <div className="example">
+      <TestComponent />
       <img
         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
         alt="a deeply impressed man, perhaps thinking 'whoa'"
